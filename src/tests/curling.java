c784e93fb2cfbf59465e20aae027b1fa88cb1294
William Weiskopf <william@weiskopf.me>
2013-11-14 12:10:37 -0700

    Tests! and eclipse.

---
+++ src/tests/curling.java
@@ -0,0 +1,81 @@
+package tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class curling {
+
+	@Before
+	public void setUp() {
+		return;
+	}
+
+	@Test
+	public void testAdvanceTurnChangePlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdvanceTurnChangeSet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdvanceTurnNewGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdvanceTurnGameOver() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testFormTeamsNumber() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testFormTeamsComposition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendPlayerStoneCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendEffect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareLess() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareEqual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareGreater() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcScorePoints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcScoreTeamScore() {
+		fail("Not yet implemented");
+	}
+}
