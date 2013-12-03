9a8778847ab12e48d05f0dd0411a988045f498e7
george <rpubaddr0@gmail.com>
2013-12-02 19:07:59 -0700

    Stones now draw themselves, and the house draws its stones.

--- src/curling/Stone.java
+++ src/curling/Stone.java
@@ -1,8 +1,15 @@
 package curling;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Polygon;
+
 public class Stone implements Comparable<Stone> {
 	private Team team;
-	private int radius, angle;
+	private Purpose purpose;
+	private int radius;
+	private double angle;
 
 	public Stone(Team team) {
 		// FIXME: Set default starting positions for stones.
@@ -12,6 +19,15 @@ public class Stone implements Comparable<Stone> {
 		radius = 0;
 		angle = 0;
 	}
+
+	/* Unsure if we need this... how are we putting stones on the rink?
+	public Stone(Team team, Purpose purpose, int x, int y) {
+		super();
+		this.team = team;
+		this.purpose = purpose;
+		setPosition(x, y);
+	}
+	*/
 
 	public Stone(Team team, int radius, int angle) {
 		super();
@@ -30,5 +46,85 @@ public class Stone implements Comparable<Stone> {
 
 	public Team getTeam() {
 		return team;
+	}
+
+	public void setPurpose(Purpose purpose) {
+		this.purpose = purpose;
+	}
+
+	public void setPosition(Point point) {
+		// Normalize (prevent stones draw offscreen due to rounding errors).
+		point.x = Math.max(point.x, 0);
+		point.y = Math.max(point.y, 0);
+		// Translate to rink 0,0.
+		point.x -= HouseLayout.circleCenterX;
+		point.y -= HouseLayout.halfRinkWidth;
+		// Convert from rectangular to polar.
+
+		radius = (int) Math.sqrt(Math.pow(point.x,  2) + Math.pow(point.y,  2));
+		angle = Math.acos((double)point.x/radius);
+	}
+
+	public void setPosition(int x, int y) {
+		// Overloaded for convenience.
+		setPosition(new Point(x, y));
+	}
+
+	public Point getPoint() {
+		// Convert from polar to rectangular.
+		Point point = new Point();
+		point.x = (int) (radius*Math.cos(angle));
+		point.y = (int) (radius*Math.sin(angle));
+		// Translate to JFrame 0,0.
+		point.x += HouseLayout.circleCenterX;
+		point.y = -point.y + HouseLayout.halfRinkWidth;
+		// Normalize (prevent stones draw offscreen due to rounding errors).
+		point.x = Math.max(point.x, 0);
+		point.y = Math.max(point.y, 0);
+
+		return point;
+	}
+
+	public int getX() {
+		return getPoint().x;
+	}
+
+	public int getY() {
+		return getPoint().y;
+	}
+
+	public void draw(Graphics g) {
+		// Define colors.
+		Color colorHome = java.awt.Color.GREEN;
+		Color colorAway = java.awt.Color.BLACK;
+		// Define shape sizes.
+		int stoneRadius = 8;
+		int x = Math.max(getX() - stoneRadius, stoneRadius);
+		int y = Math.max(getY() - stoneRadius, stoneRadius);
+		int height = stoneRadius*2;
+		int width = height;
+
+		// Set the color.
+		if (team == Team.HOME) {
+			g.setColor(colorHome);
+		} else {
+			g.setColor(colorAway);
+		}
+
+		// Draw the shape.
+		if (purpose == Purpose.DRAW) {
+			g.drawOval(x, y, width, height);
+		} else if (purpose == Purpose.TAKEOUT) {
+			g.drawRect(x, y, width, height);
+		} else if (purpose == Purpose.GUARD) {
+			Polygon triangle = new Polygon();
+			triangle.addPoint(x+stoneRadius, y);
+			triangle.addPoint(x, y+height);
+			triangle.addPoint(x+width, y+height);
+			g.drawPolygon(triangle);
+		} else {
+			// for debug purposes only!
+			g.drawString("You didn't set my purpose!", x, y);
+		}
 	}
 }
